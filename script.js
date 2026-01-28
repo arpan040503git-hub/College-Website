5e0c9a52e975b805863f31f5bf1f2447dda99c75
arpan040503git-hub <arpansinghrock@gmail.com>
2026-01-28 16:32:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function welcomeMsg() {
+  alert("Welcome to Aurora Institute of Technology College Website!");
+}
+let btn = document.getElementById("btn-apply");
+
+btn.addEventListener('click', () => {
+
+  alert("Thank You for Visiting our Site");
+})
